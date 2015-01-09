06980305cc21fbb4a4bcd54fc155a87d8e516de7
san <zhcnxf@gmail.com>
2015-01-09 14:19:44 +0800

    Initial commit

---
+++ src/main/java/taosha/lol/LolActivity.java
@@ -0,0 +1,12 @@
+package taosha.lol;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+public class LolActivity extends ActionBarActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lol);
+    }
+}
